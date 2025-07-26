513b0a16dffdfb9e107b81b99beff9baf5014dfa
kising0529 <73770815+kising0529@users.noreply.github.com>
2025-07-27 08:32:41 +0900

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,14 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true
+  },
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      external: ['firebase-admin'],
+      output: {
+        manualChunks: undefined
+      }
+    }
   }
 }) 
