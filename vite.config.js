dd4d0415da18af2364fa7d610f86ed6634f7350c
kising0529 <73770815+kising0529@users.noreply.github.com>
2025-07-27 13:02:15 +0900

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src')
-    }
-  },
   server: {
     port: 3000,
     open: true
-  },
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      external: ['firebase-admin']
-    }
   }
 }) 
